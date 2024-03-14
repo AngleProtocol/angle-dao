386791a576bae306e6256da219c0491d5073946f
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-03-14 16:43:42 -0400

    feat: angle-core project

---
+++ scripts/Utils.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "utils/src/CommonUtils.sol";
+import { console } from "forge-std/console.sol";
+import "forge-std/Script.sol";
+
+contract Utils is Script, CommonUtils {}
