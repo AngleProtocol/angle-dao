ba9dec77a1e80ba18cd92aa46dbdab4248c936ae
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-03-25 02:27:03 -0400

    refacotr: update solidity version

--- contracts/external/SmartWalletChecker.sol
+++ contracts/external/SmartWalletChecker.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.7;
+pragma solidity ^0.8.7;
 
 /// @notice Interface of the `SmartWalletChecker` contracts of the protocol
 interface SmartWalletChecker {
--- contracts/interfaces/external/compound/CTokenI.sol
+++ contracts/interfaces/external/compound/CTokenI.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.7;
+
 import "./InterestRateModel.sol";
 
 interface CTokenI {
