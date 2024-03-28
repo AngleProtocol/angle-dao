1ab62805997b7623baa2dc59950a3d250c8b9ad6
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-03-28 11:33:57 -0400

    feat: add fuzz tests for emergencyWithdrawal

---
+++ test/fuzz/EmergencyWithdrawal.t.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import { Fixture, IveANGLE } from "../Fixture.t.sol";
+import { console } from "forge-std/console.sol";
+
+contract EmergencyWithdrawalFuzz is Fixture {
+    function test_emergencyWithdrawal_Normal(
+        address[10] memory accounts,
+        uint256[10] memory balances,
+        uint256[10] memory durations
+    ) external {
+        // be sure all accounts are unique
+        for (uint i = 0; i < accounts.length; i++) {
+            vm.assume(accounts[i] != address(0));
+            for (uint j = i + 1; j < accounts.length; j++) {
+                vm.assume(accounts[i] != accounts[j]);
+            }
+        }
+
+        for (uint i = 0; i < accounts.length; i++) {
+            durations[i] = bound(durations[i], 1 weeks, 365 days * 4);
+            balances[i] = bound(balances[i], 1e18, 1e22);
+            deal(address(Angle), accounts[i], balances[i]);
+
+            vm.startPrank(accounts[i], accounts[i]);
+            Angle.approve(address(veANGLE), balances[i]);
+            veANGLE.create_lock(balances[i], block.timestamp + durations[i]);
+            vm.stopPrank();
+        }
+
+        assertEq(veANGLE.emergency_withdrawal(), false);
+
+        vm.prank(admin);
+        veANGLE.set_emergency_withdrawal();
+
+        assertEq(veANGLE.emergency_withdrawal(), true);
+
+        for (uint i = 0; i < accounts.length; i++) {
+            assertEq(Angle.balanceOf(accounts[i]), 0);
+
+            vm.startPrank(accounts[i]);
+            veANGLE.withdraw_fast();
+            vm.stopPrank();
+
+            assertEq(Angle.balanceOf(accounts[i]), balances[i]);
+        }
+    }
+
+    function test_emergencyWithdrawal_WithTimeWraps(
+        address[10] memory accounts,
+        uint256[10] memory balances,
+        uint256[10] memory durations,
+        uint256[10] memory timeWraps
+    ) external {
+        // be sure all accounts are unique
+        for (uint i = 0; i < accounts.length; i++) {
+            vm.assume(accounts[i] != address(0));
+            for (uint j = i + 1; j < accounts.length; j++) {
+                vm.assume(accounts[i] != accounts[j]);
+            }
+        }
+
+        for (uint i = 0; i < accounts.length; i++) {
+            durations[i] = bound(durations[i], 1 weeks, 365 days * 4);
+            balances[i] = bound(balances[i], 1e18, 1e22);
+            deal(address(Angle), accounts[i], balances[i]);
+
+            vm.startPrank(accounts[i], accounts[i]);
+            Angle.approve(address(veANGLE), balances[i]);
+            veANGLE.create_lock(balances[i], block.timestamp + durations[i]);
+            vm.stopPrank();
+        }
+
+        assertEq(veANGLE.emergency_withdrawal(), false);
+
+        vm.prank(admin);
+        veANGLE.set_emergency_withdrawal();
+
+        assertEq(veANGLE.emergency_withdrawal(), true);
+
+        for (uint i = 0; i < accounts.length; i++) {
+            timeWraps[i] = bound(timeWraps[i], 0, 365 days * 4 + 1);
+            assertEq(Angle.balanceOf(accounts[i]), 0);
+
+            vm.warp(block.timestamp + timeWraps[i]);
+
+            vm.startPrank(accounts[i]);
+            veANGLE.withdraw_fast();
+            vm.stopPrank();
+
+            assertEq(Angle.balanceOf(accounts[i]), balances[i]);
+        }
+    }
+}
