89a3d6b59d9cd1a073a16cb62c605cf113c10efe
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-04-20 01:33:46 -0400

    fix: script to deploy inherit VyperDeployer

--- scripts/DeployVeAngle.s.sol
+++ scripts/DeployVeAngle.s.sol
@@ -6,18 +6,16 @@ import { console } from "forge-std/console.sol";
 import { VyperDeployer } from "contracts/utils/VyperDeployer.sol";
 import "./Utils.s.sol";
 
-contract DeployVeAngleScript is Script, Utils {
+contract DeployVeAngleScript is Script, Utils, VyperDeployer {
     function run() external {
         uint256 deployerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC_MAINNET"), "m/44'/60'/0'/0/", 0);
-        vm.startBroadcast(deployerPrivateKey);
 
         address deployer = vm.addr(deployerPrivateKey);
         console.log("Deployer address: ", deployer);
 
-        VyperDeployer vyperDeployer = new VyperDeployer();
-        address veANGLE = vyperDeployer.deployContract("contracts/dao/veANGLE.vy");
-        console.log("veANGLE deployed at: ", veANGLE);
-
+        vm.startBroadcast(deployerPrivateKey);
+        address veANGLE = deployContract("contracts/dao/veANGLE.vy");
         vm.stopBroadcast();
+        console.log("veANGLE deployed at: ", veANGLE);
     }
 }
